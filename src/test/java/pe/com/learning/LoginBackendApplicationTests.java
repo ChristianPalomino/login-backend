44d572e87dd7b9b501e4300c563515e89ccb4b79
christianpalomino <christianspq@gmail.com>
2023-09-28 14:38:27 -0500

    First commit

---
+++ src/test/java/pe/com/learning/LoginBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
